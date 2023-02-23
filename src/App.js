25a9479a33ec624ae03819d27264b44159c17080
오정진 <rodang@cntech.pro>
2023-02-23 17:45:19 +0900

    feat: react-router-dom 추가

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Rank from "./pages/Rank";
 
 function App() {
-	return <Home />;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/rank" element={<Rank />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
