803f8a595f3492cb58ce7bd285dc1702245dbcc1
JUNGJIN <ojj824@naver.com>
2023-03-09 23:56:31 +0900

    feat: login modal 추가

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,12 +2,19 @@ import React from "react";
 import Layout from "../components/Layout";
 import Lottie from "lottie-react";
 import loginLottie from "../common/loginLottie.json";
+import { Modal, Button } from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
 
 function Login() {
+  const [opened, { open, close }] = useDisclosure(false);
   return (
     <Layout>
       <Lottie animationData={loginLottie} />
       <div>오늘 새벽기도 완료!</div>
+      <Button variant="gradient" gradient={{ from: "orange", to: "red" }} onClick={open}>
+        시작하기
+      </Button>
+      <Modal opened={opened} onClose={close} title="로그인하기" centered />
     </Layout>
   );
 }
