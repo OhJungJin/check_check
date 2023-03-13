92d8b0107b1a0575aff4715888720956c5360e34
오정진 <rodang@cntech.pro>
2023-03-13 09:58:45 +0900

    feat:paragraph 수정

--- src/components/Home/Paragraph.jsx
+++ src/components/Home/Paragraph.jsx
@@ -4,10 +4,10 @@ import React from "react";
 function Paragraph() {
   return (
     <Paper shadow="xl" p="md" radius="lg" mt="1rem" h="auto">
-      <Blockquote cite="– Forrest Gump">
+      <Blockquote cite="–Forrest Gump">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, aliquid.
         Suscipit placeat modi quaerat dolor fugiat! Minus nulla doloribus iusto, veniam,
-        necessitatibus debitis suscipit quam reprehenderit cumque, ex incidunt porro.
+        necessitatibus debitis suscipit quam reprehenderit cumque, ex incidunt porro.s
       </Blockquote>
     </Paper>
   );
